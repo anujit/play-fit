5242ed46a2dc5f1c0870d8b89e8a8c6718e284d1
Anujit Nene <anujitnene@gmail.com>
2016-04-20 21:06:47 +0530

    cors enabled

--- server.js
+++ server.js
@@ -91,7 +91,11 @@ app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 var router = express.Router();
 
